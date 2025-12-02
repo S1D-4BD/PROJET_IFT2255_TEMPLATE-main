c6c72eb66bcd88b692a1db5ddb81e862faa4fc19
ayadair12 <aya.dair@umontreal.ca>
2025-12-02 14:32:15 -0500

    test3 testGetMissingPrerequisites_symetricDifference() dans le fichier ComparaisonTest.java ou je teste la methode getMissingPrerequisites() qui trouve les prerequi absent du cours A dans le cours et meme chose pour le cours B

--- project/ift2255-template-javalin-main/ift2255-template-javalin-main/rest-api/src/test/model/ComparaisonTest.java
+++ project/ift2255-template-javalin-main/ift2255-template-javalin-main/rest-api/src/test/model/ComparaisonTest.java
@@ -57,9 +57,34 @@ public class ComparaisonTest {
     }
 
     // TEST: getMissingPrerequisites(), entre deux cours, trouver les prérequis qu'un cours A n'a pas et qu'un cours B a, et vice-versa
+    @Test
+    void testGetMissingPrerequisites_symetricDifference() {
+        //Arrange
+        Course courseA = new Course();
+        courseA.setId("IFT2125");
+        courseA.setPrerequisites(Arrays.asList("IFT2015", "MAT1978"));
 
+        Course courseB = new Course();
+        courseB.setId("IFT2255");
+        courseB.setPrerequisites(Arrays.asList("IFT1025"));
 
-    
+        Comparaison comparaison = new Comparaison(courseA, courseB);
+
+        //Act
+        Map<String, List<String>> missing = comparaison.getMissingPrerequisites();
+
+        List<String> missingForA = missing.get("IFT2125");
+        List<String> missingForB = missing.get("IFT2255");
+
+        //Assert
+        assertEquals(1, missingForA.size(), "Il devrait manquer 1 prérequis au cours A");
+        assertTrue(missingForA.contains("IFT1025"),
+                "Pour A, il devrait manquer le prérequis de B : IFT1025");
+
+        assertEquals(1, missingForB.size(), "Il devrait manquer 1 prérequis au cours B");
+        assertTrue(missingForB.contains("IFT2015","MAT1978"),
+                "Pour B, il devrait manquer les prérequis de A : IFT2015 et MAT1978");
+    }
 
     
 
