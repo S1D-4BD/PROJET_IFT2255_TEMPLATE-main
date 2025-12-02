a98b2c25442f1ca7dc7ef1748f9770655ed551c0
ayadair12 <aya.dair@umontreal.ca>
2025-12-02 13:36:08 -0500

    debut du test3 pour getMissingPrerequisites()

--- project/ift2255-template-javalin-main/ift2255-template-javalin-main/rest-api/src/test/model/ComparaisonTest.java
+++ project/ift2255-template-javalin-main/ift2255-template-javalin-main/rest-api/src/test/model/ComparaisonTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 public class ComparaisonTest {
    
-     //TEST: compareCredits(), tester la comparaison du nbr de credits entre deux cours (un dont les crédits ne sont pas définis)
+    //TEST: compareCredits(), tester la comparaison du nbr de credits entre deux cours (un dont les crédits ne sont pas définis)
     @Test
     void testCompareCredits_withNullAndNumericCredits() {
         //Arrange
@@ -36,7 +36,7 @@ public class ComparaisonTest {
     }
 
     
-     // TEST: getCommonSessions(), trouver l'intersection, donc la session commune dans laquelle deux cours se donne
+    // TEST: getCommonSessions(), trouver l'intersection, donc la session commune dans laquelle deux cours se donne
     @Test
     void testGetCommonSessions_simpleIntersection() {
         //Arrange
@@ -55,6 +55,10 @@ public class ComparaisonTest {
         assertEquals(1, common.size(), "Il devrait y avoir exactement 1 session commune");
         assertTrue(common.contains("H25"), "La session commune devrait être H25");
     }
+
+    // TEST: getMissingPrerequisites(), entre deux cours, trouver les prérequis qu'un cours A n'a pas et qu'un cours B a, et vice-versa
+
+
     
 
     
