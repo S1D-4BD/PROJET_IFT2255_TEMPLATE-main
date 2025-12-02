4520b5fc019de89ba79c1f8a6d29e5c6955d8562
ayadair12 <aya.dair@umontreal.ca>
2025-12-02 11:50:11 -0500

    j'ai ajoute un fichier ComparaisonTest.java, dedans j'ai ecris un test pour CompareCredits qui viens comparer le nombre de credits pour deux cours, ici je viesn comparer un cours A de 3 credits avec un cours B de 0 credits (qui n'existe pas)

---
+++ project/ift2255-template-javalin-main/ift2255-template-javalin-main/rest-api/src/test/model/ComparaisonTest.java
@@ -0,0 +1,14 @@
+package com.diro.ift2255.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+
+public class ComparaisonTest {
+    
+}
